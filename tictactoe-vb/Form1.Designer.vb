6cbc1aa40d76d880facce42fe825222fa6005974
4tonRochel <furtonrochel@gmail.com>
2022-05-23 10:48:15 +0800

    Named all the buttons

--- tictactoe-vb/Form1.Designer.vb
+++ tictactoe-vb/Form1.Designer.vb
@@ -22,132 +22,126 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
-        Me.Button9 = New System.Windows.Forms.Button()
-        Me.Button10 = New System.Windows.Forms.Button()
-        Me.Button11 = New System.Windows.Forms.Button()
-        Me.Button12 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblPlyrX = New System.Windows.Forms.Label()
+        Me.lblPlyrO = New System.Windows.Forms.Label()
+        Me.lblScoreX = New System.Windows.Forms.Label()
+        Me.lblScoreO = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btn1
         '
-        Me.Button1.Location = New System.Drawing.Point(-1, 0)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(85, 87)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn1.Location = New System.Drawing.Point(-1, 0)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(85, 87)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btn2
         '
-        Me.Button2.Location = New System.Drawing.Point(90, 0)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(85, 87)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btn2.Location = New System.Drawing.Point(90, 0)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(85, 87)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btn3
         '
-        Me.Button3.Location = New System.Drawing.Point(181, 0)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(85, 87)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btn3.Location = New System.Drawing.Point(181, 0)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(85, 87)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btn4
         '
-        Me.Button4.Location = New System.Drawing.Point(181, 93)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(85, 87)
-        Me.Button4.TabIndex = 5
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btn4.Location = New System.Drawing.Point(-1, 93)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(85, 87)
+        Me.btn4.TabIndex = 5
+        Me.btn4.UseVisualStyleBackColor = True
         '
-        'Button5
+        'btn5
         '
-        Me.Button5.Location = New System.Drawing.Point(90, 93)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(85, 87)
-        Me.Button5.TabIndex = 4
-        Me.Button5.Text = "Button5"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btn5.Location = New System.Drawing.Point(90, 93)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(85, 87)
+        Me.btn5.TabIndex = 4
+        Me.btn5.UseVisualStyleBackColor = True
         '
-        'Button6
+        'btn6
         '
-        Me.Button6.Location = New System.Drawing.Point(-1, 93)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(85, 87)
-        Me.Button6.TabIndex = 3
-        Me.Button6.Text = "Button6"
-        Me.Button6.UseVisualStyleBackColor = True
+        Me.btn6.Location = New System.Drawing.Point(181, 93)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(85, 87)
+        Me.btn6.TabIndex = 3
+        Me.btn6.UseVisualStyleBackColor = True
         '
-        'Button7
+        'btn7
         '
-        Me.Button7.Location = New System.Drawing.Point(181, 186)
-        Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(85, 87)
-        Me.Button7.TabIndex = 8
-        Me.Button7.Text = "Button7"
-        Me.Button7.UseVisualStyleBackColor = True
+        Me.btn7.Location = New System.Drawing.Point(-1, 186)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(85, 87)
+        Me.btn7.TabIndex = 8
+        Me.btn7.UseVisualStyleBackColor = True
         '
-        'Button8
+        'btn8
         '
-        Me.Button8.Location = New System.Drawing.Point(90, 186)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(85, 87)
-        Me.Button8.TabIndex = 7
-        Me.Button8.Text = "Button8"
-        Me.Button8.UseVisualStyleBackColor = True
+        Me.btn8.Location = New System.Drawing.Point(90, 186)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(85, 87)
+        Me.btn8.TabIndex = 7
+        Me.btn8.UseVisualStyleBackColor = True
         '
-        'Button9
+        'btn9
         '
-        Me.Button9.Location = New System.Drawing.Point(-1, 186)
-        Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(85, 87)
-        Me.Button9.TabIndex = 6
-        Me.Button9.Text = "Button9"
-        Me.Button9.UseVisualStyleBackColor = True
+        Me.btn9.Location = New System.Drawing.Point(181, 186)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(85, 87)
+        Me.btn9.TabIndex = 6
+        Me.btn9.UseVisualStyleBackColor = True
         '
-        'Button10
+        'btnReset
         '
-        Me.Button10.Location = New System.Drawing.Point(308, 12)
-        Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(126, 33)
-        Me.Button10.TabIndex = 9
-        Me.Button10.Text = "Reset"
-        Me.Button10.UseVisualStyleBackColor = True
+        Me.btnReset.Font = New System.Drawing.Font("Showcard Gothic", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(-1, 316)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(126, 33)
+        Me.btnReset.TabIndex = 9
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
         '
-        'Button11
+        'btnNewGame
         '
-        Me.Button11.Location = New System.Drawing.Point(308, 51)
-        Me.Button11.Name = "Button11"
-        Me.Button11.Size = New System.Drawing.Size(126, 33)
-        Me.Button11.TabIndex = 10
-        Me.Button11.Text = "New Game"
-        Me.Button11.UseVisualStyleBackColor = True
+        Me.btnNewGame.Font = New System.Drawing.Font("Showcard Gothic", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNewGame.Location = New System.Drawing.Point(-1, 355)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(126, 33)
+        Me.btnNewGame.TabIndex = 10
+        Me.btnNewGame.Text = "New Game"
+        Me.btnNewGame.UseVisualStyleBackColor = True
         '
-        'Button12
+        'btnExit
         '
-        Me.Button12.Location = New System.Drawing.Point(308, 90)
-        Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(126, 33)
-        Me.Button12.TabIndex = 11
-        Me.Button12.Text = "Exit"
-        Me.Button12.UseVisualStyleBackColor = True
+        Me.btnExit.Font = New System.Drawing.Font("Showcard Gothic", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(335, 341)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(126, 33)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -157,64 +151,69 @@ Partial Class Form1
         Me.Label1.Size = New System.Drawing.Size(0, 13)
         Me.Label1.TabIndex = 12
         '
-        'Label3
+        'lblPlyrX
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(84, 325)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(52, 13)
-        Me.Label3.TabIndex = 14
-        Me.Label3.Text = "Player X :"
+        Me.lblPlyrX.AutoSize = True
+        Me.lblPlyrX.Font = New System.Drawing.Font("Showcard Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlyrX.Location = New System.Drawing.Point(272, 19)
+        Me.lblPlyrX.Name = "lblPlyrX"
+        Me.lblPlyrX.Size = New System.Drawing.Size(110, 23)
+        Me.lblPlyrX.TabIndex = 14
+        Me.lblPlyrX.Text = "Player X :"
         '
-        'Label2
+        'lblPlyrO
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(84, 369)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
-        Me.Label2.TabIndex = 15
-        Me.Label2.Text = "Player O :"
+        Me.lblPlyrO.AutoSize = True
+        Me.lblPlyrO.Font = New System.Drawing.Font("Showcard Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlyrO.Location = New System.Drawing.Point(272, 63)
+        Me.lblPlyrO.Name = "lblPlyrO"
+        Me.lblPlyrO.Size = New System.Drawing.Size(110, 23)
+        Me.lblPlyrO.TabIndex = 15
+        Me.lblPlyrO.Text = "Player O :"
         '
-        'Label4
+        'lblScoreX
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(155, 325)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(13, 13)
-        Me.Label4.TabIndex = 16
-        Me.Label4.Text = "0"
+        Me.lblScoreX.AutoSize = True
+        Me.lblScoreX.Font = New System.Drawing.Font("Showcard Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScoreX.Location = New System.Drawing.Point(404, 19)
+        Me.lblScoreX.Name = "lblScoreX"
+        Me.lblScoreX.Size = New System.Drawing.Size(21, 23)
+        Me.lblScoreX.TabIndex = 16
+        Me.lblScoreX.Text = "0"
         '
-        'Label5
+        'lblScoreO
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(155, 369)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(13, 13)
-        Me.Label5.TabIndex = 17
-        Me.Label5.Text = "0"
+        Me.lblScoreO.AutoSize = True
+        Me.lblScoreO.Font = New System.Drawing.Font("Showcard Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScoreO.Location = New System.Drawing.Point(404, 63)
+        Me.lblScoreO.Name = "lblScoreO"
+        Me.lblScoreO.Size = New System.Drawing.Size(21, 23)
+        Me.lblScoreO.TabIndex = 17
+        Me.lblScoreO.Text = "0"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(436, 405)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label3)
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(462, 386)
+        Me.Controls.Add(Me.lblScoreO)
+        Me.Controls.Add(Me.lblScoreX)
+        Me.Controls.Add(Me.lblPlyrO)
+        Me.Controls.Add(Me.lblPlyrX)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button12)
-        Me.Controls.Add(Me.Button11)
-        Me.Controls.Add(Me.Button10)
-        Me.Controls.Add(Me.Button7)
-        Me.Controls.Add(Me.Button8)
-        Me.Controls.Add(Me.Button9)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button6)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
         Me.Name = "Form1"
         Me.Text = "Tic-Tac-Toe"
         Me.ResumeLayout(False)
@@ -222,21 +221,21 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
-    Friend WithEvents Button7 As Button
-    Friend WithEvents Button8 As Button
-    Friend WithEvents Button9 As Button
-    Friend WithEvents Button10 As Button
-    Friend WithEvents Button11 As Button
-    Friend WithEvents Button12 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnNewGame As Button
+    Friend WithEvents btnExit As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
+    Friend WithEvents lblPlyrX As Label
+    Friend WithEvents lblPlyrO As Label
+    Friend WithEvents lblScoreX As Label
+    Friend WithEvents lblScoreO As Label
 End Class
